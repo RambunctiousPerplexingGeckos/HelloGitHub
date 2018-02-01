34985f32a643bb29e53f1d09798c6547e083646e
Grace Phillips <gphi8070@students.kleinisd.net>
2018-02-01 08:40:40 -0600

    Added the while loop. May have a SMALL bug...

--- TestCode/src/HelloWorld.java
+++ TestCode/src/HelloWorld.java
@@ -4,6 +4,8 @@ import java.util.*;
 //push puts them into the actual repository
 public class HelloWorld {
 	public static void main(String[] args) {
+		while (8<9) {
 		System.out.println("Hello, world!");
+		}
 	}
 }
