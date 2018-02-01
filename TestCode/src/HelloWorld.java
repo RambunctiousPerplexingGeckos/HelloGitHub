2117a55d110b79dfa094e5320ae8ecf9a9ea2e43
S577807 <S577807@003-TZC-291D35.kleinisd.loc>
2018-02-01 08:38:49 -0600

    Started the project and began to print "Hello, world!".

---
+++ TestCode/src/HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.*;
+@SuppressWarnings("unused")
+//commit saves a version
+//push puts them into the actual repository
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
