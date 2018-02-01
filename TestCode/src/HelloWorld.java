20948753473bcbade8fefee697c29f1be15de5f0
S577807 <S577807@003-TZC-291D35.kleinisd.loc>
2018-02-01 08:58:27 -0600

    My first Commit and Push! Fixed the infinite loop. Should it
    have a more intuitive stop command?

--- TestCode/src/HelloWorld.java
+++ TestCode/src/HelloWorld.java
@@ -4,8 +4,14 @@ import java.util.*;
 //push puts them into the actual repository
 public class HelloWorld {
 	public static void main(String[] args) {
-		while (8<9) {
+		Scanner i = new Scanner(System.in);
+		boolean done = false;
+		while (!done) {
 		System.out.println("Hello, world!");
+		if (i.nextLine().equalsIgnoreCase("STOP")){
+			done = true;
+			}
 		}
+		i.close();
 	}
 }
